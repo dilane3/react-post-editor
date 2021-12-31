ac654c5349493e9beb2c087688001e748d67e2aa
dilane3 <komboudilane125@gmail.com>
2021-12-31 12:45:44 +0100

    setting of text editor for post and adding of emojis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState } from 'react';
+import EmojiCard from './EmojiCard';
+import "./App.css"
 
-function App() {
+const image = require("./boules2.png")
+
+const App = () => {
+  const [showEmoji, setShowEmoji] = useState(false)
+  const [post, setPost] = useState("")
+  // const [img, setImage] = useState(null)
+
+  const updatePost = (event) => {
+    setPost(event.target.value)
+  }
+
+  const addEmoji = (emoji) => {
+    // setPost(post + emoji.emoji)
+    let messageEditor = document.querySelector('.text-editor');
+
+    console.log(messageEditor.selectionStart);
+    messageEditor.setRangeText(`${emoji.emoji}`, messageEditor.selectionStart, messageEditor.selectionEnd, "end");
+
+    setPost(messageEditor.value);
+
+    messageEditor.focus();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className="container">
+      <section className="container-top">
+        <div className="container-img">
+          <img src={image} alt="" />
+        </div>
+
+        <div className="container-text">
+          <textarea 
+            className="text-editor" 
+            placeholder="Write a post"
+            value={post}
+            onChange={updatePost}
+          >
+          </textarea>
+
+          {/* <div className="images">
+            {
+              img && <img src={img} />
+            }
+            <img src={image} alt="post" />
+          </div> */}
+        </div>
+      </section>
+
+      <div className="icon-section">
+        <div>
+          <i className="bi bi-emoji-smile" onClick={() => {setShowEmoji(!showEmoji)}}></i>
+          <i className="bi bi-image"></i>
+        </div>
+
+        <button className="btn-publier">Publier</button>
+
+        <EmojiCard 
+          show={showEmoji} 
+          animationShow="emoji-card-show"
+          getEmoji={addEmoji}
+        />
+      </div>
+    </article>
   );
-}
+};
 
 export default App;
---
+++ src/EmojiCard.js
@@ -0,0 +1,29 @@
+import React, {useRef} from 'react';
+import Picker from 'emoji-picker-react';
+import "./App.css"
+
+const EmojiCard = ({show, getEmoji, animationShow}) => {
+  const sectionRef = useRef()
+
+  const onEmojiClick = (event, emojiObject) => {
+    getEmoji(emojiObject);
+  };
+
+  return (
+    <section 
+      ref={sectionRef}
+      className={`emoji-card ${animationShow}`}
+      style={{ display: show ? "block":"none" }}
+    >
+      <Picker 
+        onEmojiClick={onEmojiClick}
+        pickerStyle={{ 
+          boxShadow: "0 2px 4px rgb(143, 142, 142)" ,
+          fontFamily: "ubuntu"
+        }}
+      />
+    </section>
+  );
+};
+
+export default EmojiCard;
